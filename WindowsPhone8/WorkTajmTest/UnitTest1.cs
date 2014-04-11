d46d764e585479e0a5a6ec6537a49e6927476bbe
Jim Arnell <jim@arnellconsulting.com>
2014-04-12 00:33:39 +0200

    Unit test project added.

---
+++ WindowsPhone8/WorkTajmTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace WorkTajmTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
