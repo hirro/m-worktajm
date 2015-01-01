98b0da59163d2f0b051c17d6dd5980b109fb2078
Jim Arnell <jim@arnellconsulting.com>
2015-01-01 22:55:00 +0100

    no message

--- ios/Worktajm/WorktajmTests/WorktajmTests.swift
+++ ios/Worktajm/WorktajmTests/WorktajmTests.swift
@@ -2,8 +2,8 @@
 //  worktajmTests.swift
 //  worktajmTests
 //
-//  Created by Jim Arnell on 2014-12-26.
-//  Copyright (c) 2014 Arnell Consulting AB. All rights reserved.
+//  Created by Jim Arnell on 01/01/15.
+//  Copyright (c) 2015 Arnell Consulting AB. All rights reserved.
 //
 
 import UIKit
