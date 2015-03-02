048bd547c5b635a7f84bb86e880fd8470b18bb07
Jim Arnell <jim@arnellconsulting.com>
2015-03-02 08:46:39 +0100

    no message

---
+++ ios/Worktajm/Worktajm/ProjectTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProjectTableViewCell.swift
+//  worktajm
+//
+//  Created by Jim Arnell on 21/01/15.
+//  Copyright (c) 2015 Arnell Consulting AB. All rights reserved.
+//
+
+import UIKit
+
+class ProjectTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
